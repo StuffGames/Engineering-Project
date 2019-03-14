9990071c043f7708079d83b5591778cce137aafe
StuffGames <44318828+StuffGames@users.noreply.github.com>
2019-03-13 21:55:56 -0400

    Base Game Project
    This is the base of the game project since last used in room 406 on 3/12/2019. This is gonna be the original file to use for the  base of all changes even though it is not the starting empty project, it will do for future changes whether big or small. This is to be transferred between computers and unfortunately Operating Systems.

---
+++ Engineering Project/Assets/CharacterContollerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterContollerScript : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public Transform groundCheck;
+    public Transform wallCheck;
+    public LayerMask whatIsGround;
+
+    public float speed = 5f;
+    public float jumpForce = 500f;
+    private float radius = 0.3f;
+    private bool grounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float playerRight = transform.right.x;
+        rb.velocity = new Vector2(playerRight * speed, rb.velocity.y);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, radius, whatIsGround);
+
+        if (grounded && Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+        }
+    }
+}
